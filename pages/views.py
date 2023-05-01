9f2772f203de55b52324bd5c85e5cbf1835d2a1a
Ibrokhimbek <ibrohim.pulatov@gmail.com>
2023-05-01 16:15:35 +0500

    added functions for subcategory and product detail

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Product, Category, Subcategory
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home_view(request):
     return render(request, "pages/index.html")
 
 def shop_view(request):
-    return render(request, "pages/shop.html")
+    products = Product.objects.all()
+    paginator = Paginator(products, 2)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
+    context = {
+        "products": result
+    }
+    return render(request, "pages/shop.html", context)
+
+def subcategory_articles_view(request, slug):
+    subcategory = Subcategory.objects.get(slug=slug)
+    products = Product.objects.filter(subcategory=subcategory)
+    paginator = Paginator(products, 2)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
+    context = {
+        "products": result
+    }
+    return render(request, "pages/shop.html", context)
+
+def product_detail_view(request, slug):
+    return render(request, "pages/product_detail.html")
