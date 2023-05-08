5f465222ac40e2c9ee4a99c1a01b055ce285b00b
Ibrokhimbek <ibrohim.pulatov@gmail.com>
2023-05-08 16:27:34 +0500

    added custome_user and accounts

--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, HttpResponse
 from .models import Product, Category, Subcategory
 from django.core.paginator import Paginator
+
+
 # Create your views here.
 
 def home_view(request):
     return render(request, "pages/index.html")
+
 
 def get_paginator(request, queryset):
     paginator = Paginator(queryset, 2)
     page = request.GET.get("page")
     result = paginator.get_page(page)
     return result
+
 
 def shop_view(request):
     products = Product.objects.all()
@@ -19,6 +23,7 @@ def shop_view(request):
         "products": result
     }
     return render(request, "pages/shop.html", context)
+
 
 def subcategory_articles_view(request, slug):
     subcategory = Subcategory.objects.get(slug=slug)
@@ -29,10 +34,14 @@ def subcategory_articles_view(request, slug):
     }
     return render(request, "pages/shop.html", context)
 
+
 def product_detail_view(request, slug):
     product = Product.objects.get(slug=slug)
+    category = product.category
+    related_products = Product.objects.filter(category=category).order_by("?")
+    # related_products = [item for item in related_products if item.pk != product.pk]
     context = {
-        "product": product
+        "product_detail": product,
+        "related_products": related_products[:4]
     }
     return render(request, "pages/product_detail.html", context)
-
